77550f27036a1b8bc745984569a8d40975adba9e
Cadiducho <cadiducho@gmail.com>
2014-09-05 12:59:45 +0200

    Episodio 1.2
    Parcheamos un poco el código para su mejor optimización

--- Java/com/cadiducho/dev/Tutorial/Tutorial.java
+++ Java/com/cadiducho/dev/Tutorial/Tutorial.java
@@ -1,4 +1,3 @@
-
 package com.cadiducho.dev.Tutorial;
 
 
@@ -12,34 +11,33 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Tutorial extends JavaPlugin {
        
         private MySQL mysql = null;
-        private Connection conection = null;
+        private Connection connection = null;
+        
         @Override
         public void onEnable() {
             File config = new File(getDataFolder() + File.separator + "config.yml");
             if (!config.exists()){
                 getLogger().info("Creando config");
-                this.getConfig().addDefault("database", "statsbukkit");
-                this.getConfig().addDefault("username", "cadi");
-                this.getConfig().addDefault("password", "pass");
-                this.getConfig().addDefault("host", "localhost");
-                this.getConfig().addDefault("port", "3306");
-                this.getConfig().options().copyDefaults(true);
-                this.saveConfig();
-                
-            } else {
-                try{
-                    mysql = new MySQL(this, this.getConfig().getString("host"), 
-                            this.getConfig().getString("port"),
-                            this.getConfig().getString("database"),
-                            this.getConfig().getString("username"),
-                            this.getConfig().getString("password"));
-                    conection = mysql.openConnection();
-                    MySQL.crearTabla(mysql, conection, this);
-                    
-                    
+                try {
+                    getConfig().options().copyDefaults(true);
+                    saveConfig();
+                    getLogger().info("Se ha generado el config.yml correctamente");
                 } catch (Exception e){
-                  this.getLogger().severe("Fallo al establecer la conexion a MySQL");
+                    getLogger().severe("Error al generar el config.yml");
                 }
+            }
+            /* 
+             * ABRIR MYSQL
+             */
+            try {
+                mysql = new MySQL(this, this.getConfig().getString("mysql.host"), this.getConfig().getString("mysql.port"),
+					this.getConfig().getString("mysql.database"), this.getConfig().getString("mysql.username"),
+					this.getConfig().getString("mysql.password"));
+                connection = mysql.openConnection();
+                //Crear tabla si no existe
+                MySQL.crearTabla(mysql, connection, this);
+            } catch (Exception e){
+                getLogger().severe("No se ha podido abrir la conexion MySQL");
             }
         }
         @Override
@@ -50,5 +48,12 @@ public class Tutorial extends JavaPlugin {
         @Override
         public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
            return false;
+        }
+        
+        public Connection getConnection(){
+            return this.connection;
+        }
+        public MySQL getMySQL(){
+            return this.mysql;
         }
 }
